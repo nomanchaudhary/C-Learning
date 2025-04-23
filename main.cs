64eea408d1dadd4c6b7a1d960dc936a57ec17de2
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-04-24 01:28:54 +0500

    first push

---
+++ main.cs
@@ -0,0 +1,9 @@
+﻿class MainProgram
+{
+    public static void Main()
+    {
+        int number = 12;
+        string Myname = "Noman Chaudhary";
+        Console.WriteLine("The Number is = "+ number + Myname);
+    }
+}
