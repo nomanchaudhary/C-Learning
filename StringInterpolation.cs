41b4dc208ba3944a243425c23fc7340f19a462b9
nomanchaudhary <nomanchaudhary1618@gmail.com>
2025-04-24 23:44:27 +0500

    New Topics Covered

---
+++ StringInterpolation.cs
@@ -0,0 +1,9 @@
+class StringInterpolation{
+
+    public static void Main(){
+
+        int age = 42;
+        string str = $"My age is {age}"; //string interpolation
+        Console.WriteLine(str);
+    }
+}
---
+++ stringformat.cs
@@ -0,0 +1,10 @@
+// class StringFormat{
+
+//     public static void Main(){
+
+//         int age = 22;
+//         string name = "Muhammad Noman";
+//         string str = string.Format("Hey {0}! You are {1} years old.", name, age);
+//         Console.WriteLine(str);
+//     }
+// }
